aad641b96a99ce30e3ef3f94c61c685bb2e0be73
ogatakatsuya <katsuya151225@icloud.com>
2025-06-27 07:55:47 +0900

    add: astro-code-expressive

--- astro.config.ts
+++ astro.config.ts
@@ -1,23 +1,28 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 import mdx from '@astrojs/mdx';
- 
+import expressiveCode from 'astro-expressive-code';
+
 // https://astro.build/config
 export default defineConfig({
     site: 'https://ogatakatsuya.com',
     vite: {
         plugins: [tailwindcss()],
     },
-    integrations: [react(), sitemap(), mdx()],
-    markdown: {
-        shikiConfig: {
-          themes: {
-            light: 'gruvbox-light-hard',
-            dark: 'gruvbox-dark-hard',
-          },
+    integrations: [
+      react(), 
+      sitemap(), 
+      expressiveCode({
+        defaultProps: {
+          wrap: false,
+          preserveIndent: true,
         },
-      },
+        themes: ['everforest-dark', 'everforest-light'],
+      }), 
+      mdx()
+    ],
 });
