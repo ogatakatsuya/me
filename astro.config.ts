0143e67fa2408b2f3a2a164e188b6692fd5dedb7
ogatakatsuya <katsuya151225@icloud.com>
2025-06-27 08:38:49 +0900

    add: sns icons

--- astro.config.ts
+++ astro.config.ts
@@ -7,22 +7,19 @@ import sitemap from '@astrojs/sitemap';
 import mdx from '@astrojs/mdx';
 import expressiveCode from 'astro-expressive-code';
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
     site: 'https://ogatakatsuya.com',
     vite: {
         plugins: [tailwindcss()],
     },
-    integrations: [
-      react(), 
-      sitemap(), 
-      expressiveCode({
-        defaultProps: {
-          wrap: false,
-          preserveIndent: true,
-        },
-        themes: ['everforest-dark', 'everforest-light'],
-      }), 
-      mdx()
-    ],
+    integrations: [react(), sitemap(), expressiveCode({
+      defaultProps: {
+        wrap: false,
+        preserveIndent: true,
+      },
+      themes: ['everforest-dark', 'everforest-light'],
+    }), mdx(), icon()],
 });
